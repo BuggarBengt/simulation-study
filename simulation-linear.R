fc50f4e44ad5e1fb8ce2509b7c38add8f3db868b
Joakim Wallmark <wallmark.joakim@gmail.com>
2019-10-11 12:55:21 +0200

    split up functions to one file to be able to source easier. Tried to get realistic scenario but nothing is significant so I failed :D

--- simulation-linear.R
+++ simulation-linear.R
@@ -1,86 +1,49 @@
-library(sensmediation)
 library(ggplot2)
+library(mediation)
 
+data(jobs) #let's use this our "true" scenario
+model_exposure <- glm(treat ~ age, data = jobs, family = binomial(link = "probit"))
+model_mediator <- glm(job_seek ~ treat + age, data = jobs)
+model_outcome  <- glm(depress2 ~ treat * job_seek + age , data = jobs)
+coef(model_exposure) 
+coef(model_mediator) 
+coef(model_outcome) #true correlation = 0.0653195710 
+test.my <- sensmediation(med.model=model_mediator, out.model=model_outcome, Rho=0, progress=FALSE, exp.name = "treat", med.name = "job_seek")
+test.my$NIE
+test.my$NDE
+test.my$CI$CI.nie[,1]
+test.my$CI$CI.nie[,2]
+test.my$CI$CI.nde[,1]
+test.my$CI$CI.nde[,2]
+#coefficents to be used as true
+exposure.coefs = c(a0 = 0.35512882, a1 = 0.00206941)
+mediator.coefs = c(b0 = 3.827541111, b1 = 0.065751007, b2 = 0.004576608, b3 = 0) 
+outcome.coefs = c(t0 = 2.8405520021, t1 = -0.3108865465, t2 = -0.2717585224, t3=corr.coef[i], t4 = 0.0007960718, t5 = 0, t6 = 0, t7 = 0)
 
-calc.nde.linear = function(z.from, z.to, x, b, t) {
-  return((t[2] + t[4]*b[1] + t[4]*b[2]*z.from + t[4]*b[3]*x)*(z.to-z.from))
-}
-
-calc.nie.linear = function(z.from, z.to, b, t) {
-  return((t[3]*b[2] + t[4]*b[2]*z.to)*(z.to-z.from))
-}
-
-runSimulation = function(S, n, exposure.coefs, mediator.coefs, outcome.coefs, exposure.error.sd = 1, mediator.error.sd = 1, outcome.error.sd = 1) {
-  # Storage of results
-  effekter.nie <- rep(NA, S) # Estimated NIE 
-  effekter.nde <- rep(NA, S) # Estimated NDE 
-  CI.nie.lower <- rep(NA, S) # Lower bound of 95% CI of NIE  
-  CI.nie.upper <- rep(NA, S) # Upper bound of 95% CI of NIE
-  CI.nde.lower <- rep(NA, S) # Lower bound of 95% CI of NDE
-  CI.nde.upper <- rep(NA, S) # Upper bound of 95% CI of NDE
-  nde.S <- numeric(S) # True NDE
-  nie.S = calc.nie.linear(z.from = 0, z.to = 1, b = mediator.coefs, t = outcome.coefs) # True NIE 
-  
-  for(i in 1:S){
-    if(i %% 50 == 0)
-      print(i)
-    
-    z.epsilon <- rnorm(n, sd = exposure.error.sd) # Error terms for each model.
-    m.epsilon <- rnorm(n, sd = mediator.error.sd) 
-    y.epsilon <- rnorm(n, sd = outcome.error.sd) 
-    
-    X <- 104-rgamma(n,shape=7,scale=4.5) # "age" 
-    
-    # Generate exposure, mediator and outcome (True models):
-    Z.star <- exposure.coefs["a0"] + exposure.coefs["a1"]*X + z.epsilon
-    Z <- ifelse(Z.star>0, 1, 0)
-    M <- mediator.coefs["b0"] + mediator.coefs["b1"]*Z + mediator.coefs["b2"]*X + m.epsilon
-    Y <- outcome.coefs["t0"] + outcome.coefs["t1"]*Z + outcome.coefs["t2"]*M + outcome.coefs["t3"]*Z*M + outcome.coefs["t4"]*X + y.epsilon
-    
-    nde.S[i] = mean(calc.nde.linear(z.from = 0, z.to = 1, b = mediator.coefs, t = outcome.coefs, x = X))
-    
-    # Estimated models (y.model misspecified without Z-M correlation):
-    m.model <- glm(M ~ Z + X) 
-    y.model <- glm(Y ~ Z + M + X)
-    
-    # Estimation of effects:
-    test.my <- sensmediation(med.model=m.model, out.model=y.model, Rho=0, progress=FALSE, exp.name = "Z", med.name = "M")
-    
-    # Storage of results
-    effekter.nie[i] <- test.my$NIE
-    effekter.nde[i] <- test.my$NDE
-    CI.nie.lower[i] <- test.my$CI$CI.nie[,1]
-    CI.nie.upper[i] <- test.my$CI$CI.nie[,2]
-    CI.nde.lower[i] <- test.my$CI$CI.nde[,1]
-    CI.nde.upper[i] <- test.my$CI$CI.nde[,2]
-  }  
-  
-  return(list(true.nie = nie.S,
-              true.nde = nde.S,
-              est.nie = effekter.nie, 
-              est.nde = effekter.nde, 
-              CI.nie.lower = CI.nie.lower,
-              CI.nie.upper = CI.nie.upper,
-              CI.nde.lower = CI.nde.lower,
-              CI.nde.upper = CI.nde.upper))
-}
+summary(jobs) # we want a closeish resemblance of age in jobs using the gamma function
+summary(rgamma(2000,shape=8,scale=4.5))
+summary(jobs$age)
+hist(rgamma(200,shape=8,scale=4.5)) # we put this as a generator for age when running simulations
+hist(jobs$age)
 
 set.seed(4235)
-corr.coef = seq(0, 5, 0.1)
+corr.coef = seq(0, 1, 0.02)
 to.plot = matrix(nrow = length(corr.coef), ncol = 7) #preallocate vector to plot
-colnames(to.plot) = c("interaction.coefficient", "nde.bias", "nie.bias", "nde.percent.bias", "nie.percent.bias","nde.coverage", "nie.coverage")
+colnames(to.plot) = c("interaction.coefficient", "est.nde", "est.nie", "nde.bias", "nie.bias", "nde.percent.bias", "nie.percent.bias","nde.coverage", "nie.coverage")
 result = vector(mode = "list", length = length(corr.coef))  #preallocate result list
 for (i in 1:length(result)) { # run simulations while increasing interaction effect
   result[[i]] = runSimulation(S = 100, n = 1000, exposure.coefs = c(a0 = -3.416096, a1 = 0.036231), 
-                         mediator.coefs = c(b0 = 10, b1 = 1.4, b2 = 0.011, b3 = 0), 
-                         outcome.coefs = c(t0 = 10, t1 = 0.5, t2 = 0.15, t3=corr.coef[i], t4 = 0.035, t5 = 0, t6 = 0, t7 = 0))
+                              mediator.coefs = c(b0 = 10, b1 = 1.4, b2 = 0.011, b3 = 0), 
+                              outcome.coefs = c(t0 = 10, t1 = 0.5, t2 = 0.15, t3=corr.coef[i], t4 = 0.035, t5 = 0, t6 = 0, t7 = 0))
   to.plot[i, 1] = corr.coef[i]
   to.plot[i, 2] = mean(result[[i]]$est.nde) - mean(result[[i]]$true.nde)
   to.plot[i, 3] = mean(result[[i]]$est.nie) - result[[i]]$true.nie
-  to.plot[i, 4] = (mean(result[[i]]$est.nde) - mean(result[[i]]$true.nde))/mean(result[[i]]$true.nde)
-  to.plot[i, 5] = (mean(result[[i]]$est.nie) - result[[i]]$true.nie)/result[[i]]$true.nie
-  to.plot[i, 6] = mean(ifelse(result[[i]]$true.nde < result[[i]]$CI.nde.upper & result[[i]]$true.nde > result[[i]]$CI.nde.lower, 1, 0))
-  to.plot[i, 7] = mean(ifelse(result[[i]]$true.nie < result[[i]]$CI.nie.upper & result[[i]]$true.nie > result[[i]]$CI.nie.lower, 1, 0))
+  to.plot[i, 4] = mean(result[[i]]$est.nde) - mean(result[[i]]$true.nde)
+  to.plot[i, 5] = mean(result[[i]]$est.nie) - result[[i]]$true.nie
+  to.plot[i, 6] = (mean(result[[i]]$est.nde) - mean(result[[i]]$true.nde))/mean(result[[i]]$true.nde)
+  to.plot[i, 7] = (mean(result[[i]]$est.nie) - result[[i]]$true.nie)/result[[i]]$true.nie
+  to.plot[i, 8] = mean(ifelse(result[[i]]$true.nde < result[[i]]$CI.nde.upper & result[[i]]$true.nde > result[[i]]$CI.nde.lower, 1, 0))
+  to.plot[i, 9] = mean(ifelse(result[[i]]$true.nie < result[[i]]$CI.nie.upper & result[[i]]$true.nie > result[[i]]$CI.nie.lower, 1, 0))
 }
 ggplot(data.frame(to.plot), aes(x=interaction.coefficient, y = nde.bias)) +
   geom_point() +
@@ -113,3 +76,4 @@ NIE.string = paste("True NIE: ", round(mean(result$true.nie), 4), " Est. NIE: ",
 NDE.string = paste("True NDE: ", round(mean(result$true.nde), 4), " Est. NDE: ", round(mean(result$est.nde), digits = 4), " Avg. CI width: (", round(mean(result$CI.nde.lower), digits = 4), "; ", round(mean(result$CI.nde.upper), digits = 4), ")", sep = "") 
 NIE.string
 NDE.string
+
